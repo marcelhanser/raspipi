0b43984ed0f78ba8dd480a4040a6b94ee33b820d
Marcel Hanser <nanotec@gmx.de>
2018-09-14 22:15:34 +0200

    add sonos control

--- src/Displayer.js
+++ src/Displayer.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import request from 'superagent';
 import logo from './logo.svg';
 import train from './train.svg';
+import playButton from './play-button.svg';
+import pauseButton from './pause-button.svg';
 import './App.css';
 
 class Displayer extends Component {
@@ -36,6 +38,16 @@ class Displayer extends Component {
         }, 1000);
     };
 
+    sonosPlay = () => {
+        const url = 'http://localhost:5005/wohnzimmer/play';
+        request.get(url).end();
+    };
+
+    sonosPause = () => {
+        const url = 'http://localhost:5005/wohnzimmer/pause';
+        request.get(url).end();
+    };
+
     render() {
         const {background, fontcolor, text, giphy} = this.state;
         return (
@@ -43,7 +55,9 @@ class Displayer extends Component {
 
                 <header className="App-header" style={{background, color: fontcolor}}>
                     <img src={logo} className="App-logo" alt="logo" onClick={() => window.location = "/poster"}/>
-                    <img src={train} id="train" alt="train" onClick={() => window.location = "/zvv"}/>
+                    <img src={train} className="turningIcon" alt="train" onClick={() => window.location = "/zvv"}/>
+                    <img src={playButton} className="turningIcon" onClick={this.sonosPlay}/>
+                    <img src={pauseButton} className="turningIcon" onClick={this.sonosPause}/>
                     <h1>{text}</h1>
                     <div>
                         <iframe src={giphy} height={200}/>
